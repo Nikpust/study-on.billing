c1537bfae3decc7f5719b4a9d289f8c683b0f726
Nikita <20_nik_05@mail.ru>
2026-04-16 20:20:40 +0300

    feat: реализованы фикстуры для User

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher
+    ) {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $usersData = [
+            [
+                'email' => 'test-user@mail.ru',
+                'password' => 'user-password',
+            ],
+            [
+                'email' => 'test-admin@mail.ru',
+                'password' => 'admin-password',
+                'roles' => ['ROLE_SUPER_ADMIN'],
+            ],
+        ];
+
+        foreach ($usersData as $userData) {
+            $user = new User();
+
+            $user->setEmail($userData['email']);
+            $user->setPassword($this->passwordHasher->hashPassword($user, $userData['password']));
+
+            if (isset($userData['roles'])) {
+                $user->setRoles($userData['roles']);
+            }
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
